430abf5b1bdff5730752de1b59881190bfedf6a7
Pierre Carion <pcarion@gmail.com>
2025-01-03 17:43:08 -0800

    ui experiments

--- cmd/cleric/main.go
+++ cmd/cleric/main.go
@@ -1,30 +1,54 @@
 package main
 
 import (
-	"image/color"
-	"time"
-
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
-	"fyne.io/fyne/v2/canvas"
+	"fyne.io/fyne/v2/widget"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/layout"
 )
+
+var data = []string{"a", "string", "list"}
 
 func main() {
 	myApp := app.New()
-	myWindow := myApp.NewWindow("Canvas")
-	myCanvas := myWindow.Canvas()
+	myWindow := myApp.NewWindow("List Widget")
 
-	blue := color.NRGBA{R: 0, G: 0, B: 180, A: 255}
-	rect := canvas.NewRectangle(blue)
-	myCanvas.SetContent(rect)
+	// Get window size and calculate desired width
+	windowWidth := float32(800) // default reasonable width
 
-	go func() {
-		time.Sleep(time.Second)
-		green := color.NRGBA{R: 0, G: 180, B: 0, A: 255}
-		rect.FillColor = green
-		rect.Refresh()
-	}()
+	// Set window size (using the calculated width and a reasonable height)
+	myWindow.Resize(fyne.NewSize(windowWidth, 400))
 
-	myWindow.Resize(fyne.NewSize(100, 100))
+	// Center the window on screen
+	myWindow.CenterOnScreen()
+
+	
+	list := widget.NewList(
+		func() int {
+			return len(data)
+		},
+		func() fyne.CanvasObject {
+			return container.NewBorder(
+				nil, nil, 
+				widget.NewLabel("template"),
+				widget.NewCheck("", func(bool) {}),
+			)
+		},
+		func(i widget.ListItemID, o fyne.CanvasObject) {
+			cont := o.(*fyne.Container)
+			label := cont.Objects[0].(*widget.Label)
+			label.SetText(data[i])
+		})
+
+	buttons := container.New(layout.NewGridLayout(3),
+		widget.NewButton("Button 1", func() {}),
+		widget.NewButton("Button 2", func() {}),
+		widget.NewButton("Button 3", func() {}),
+	)
+
+	content := container.NewBorder(nil, buttons, nil, nil, list)
+	
+	myWindow.SetContent(content)
 	myWindow.ShowAndRun()
 }
