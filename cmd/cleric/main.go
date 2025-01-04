0a570c47cec295332adf7ba9f5d4df789dbe92dd
Pierre Carion <pcarion@gmail.com>
2025-01-03 17:47:06 -0800

    style checkbox

--- cmd/cleric/main.go
+++ cmd/cleric/main.go
@@ -6,12 +6,26 @@ import (
 	"fyne.io/fyne/v2/widget"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/layout"
+	"fyne.io/fyne/v2/theme"
+	"image/color"
 )
 
 var data = []string{"a", "string", "list"}
 
+type customTheme struct {
+	fyne.Theme
+}
+
+func (t *customTheme) Color(name fyne.ThemeColorName, variant fyne.ThemeVariant) color.Color {
+	if name == theme.ColorNameInputBorder {
+		return color.RGBA{R: 255, G: 255, B: 255, A: 255}
+	}
+	return theme.DefaultTheme().Color(name, variant)
+}
+
 func main() {
 	myApp := app.New()
+	myApp.Settings().SetTheme(&customTheme{theme.DefaultTheme()})
 	myWindow := myApp.NewWindow("List Widget")
 
 	// Get window size and calculate desired width
@@ -29,15 +43,17 @@ func main() {
 			return len(data)
 		},
 		func() fyne.CanvasObject {
+			check := widget.NewCheck("", func(bool) {})
 			return container.NewBorder(
 				nil, nil, 
 				widget.NewLabel("template"),
-				widget.NewCheck("", func(bool) {}),
+				check,
 			)
 		},
 		func(i widget.ListItemID, o fyne.CanvasObject) {
 			cont := o.(*fyne.Container)
 			label := cont.Objects[0].(*widget.Label)
+			
 			label.SetText(data[i])
 		})
 
