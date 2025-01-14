0bb5307992a3e93b85e1523ad74b4938223ec6bb
Pierre Carion <pcarion@gmail.com>
2025-01-14 07:42:06 -0800

    kill with process group

--- internal/ui/inspectorDialog.go
+++ internal/ui/inspectorDialog.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"os/exec"
 	"strings"
+	"syscall"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/container"
@@ -60,6 +61,7 @@ type CommandRunner struct {
 
 func runCommand(name string, args []string, onOutput func(line string), onUrl func(url string)) (*CommandRunner, error) {
 	cmd := exec.Command(name, args...)
+	cmd.SysProcAttr = &syscall.SysProcAttr{Setpgid: true}
 
 	// Set up pipes for stdout and stderr
 	stdout, err := cmd.StdoutPipe()
@@ -138,6 +140,8 @@ func (cr *CommandRunner) Kill() {
 	cr.stdout.Close()
 	cr.stderr.Close()
 	if cr.cmd != nil && cr.cmd.Process != nil {
+		// Kill the entire process group
+		syscall.Kill(-cr.cmd.Process.Pid, syscall.SIGKILL)
 		cr.cmd.Process.Kill()
 	}
 }
